20162d63504d072cd6d972da38c41fc8e113644c
sdt-soufian <sdt.soufian@gmail.com>
2021-08-05 22:11:27 +0100

    DOM optimization and form validation

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ var btn_calculate = document.getElementById("btn-budget");
 var inputText = document.getElementById("expense-text");
 var inputAmount = document.getElementById("Amount");
 var btn_expense = document.getElementById("btn-expense");
-var tab = document.querySelector("table");
+var tbody = document.getElementById('tab-body');
 var title_budget = document.getElementById("budget-price");
 var title_expence = document.getElementById("expences-price");
 var title_balance = document.getElementById("balance-price");
 var message = document.querySelector(".msg-error");
-var tbody = document.createElement("tbody");
 var pExpence = 0;
 var pBalance = null;
+var tabItems = [];
 
-//Ecouter le événements
+//Ecoutez les événements
 btn_calculate.addEventListener("click", budgetCalculate);
 btn_expense.addEventListener("click", expenseCalculate);
 
@@ -33,76 +33,75 @@ function budgetCalculate(e) {
   } else {
     title_budget.textContent = inputBudget.value;
     title_balance.textContent = inputBudget.value;
+    inputBudget.value = "";
   }
 }
 
-/**
- * 1. There is no form validation when this function is called!
- * which generates few errors :
- *
- * - EXPENCES = NaN
- * - BALANCE = NaN
- * - Creates an empty expenses row
- *
- * TODO: Validate field type & value
- *
- * 2. No error messages are shown in case of invalid input
- * TODO: Show error messages
- *
- * 3. Fields keep their value after a successful calculation
- * TODO: Empty the fields if they are valid
- */
 function expenseCalculate(e) {
   e.preventDefault();
-  var tabINfo = [inputText.value, inputAmount.value];
-  pExpence += parseInt(tabINfo[1]);
-  title_expence.textContent = pExpence;
-  title_balance.textContent = (
-    parseInt(title_balance.textContent) - parseInt(tabINfo[1])
-  ).toString();
-  var tr = document.createElement("tr");
-  tabINfo.forEach((elet) => {
-    var td = document.createElement("td");
-    td.textContent = elet;
-    tr.appendChild(td);
-  });
-  var td = document.createElement("td");
-  var btn_edit = document.createElement("button");
-  btn_edit.classList.add("btn-style", "edit-color");
-  btn_edit.addEventListener("click", editRow);
-  btn_edit.innerHTML = '<i class="fas fa-edit"></i>';
-  var btn_delete = document.createElement("button");
-  btn_delete.classList.add("btn-style", "delete-color");
-  btn_delete.addEventListener("click", deleteRow);
-  btn_delete.innerHTML = '<i class="fas fa-trash"></i>';
-  td.appendChild(btn_edit);
-  td.appendChild(btn_delete);
-  tr.appendChild(td);
-  tbody.appendChild(tr);
-  tab.appendChild(tbody);
-  inputText.value = " ";
-  inputAmount.value = "";
+  if (inputAmount.value == "" || inputAmount.value < 0 || inputText.value == " ") {
+    message.classList.add("msg-show");
+    setTimeout(() => {
+      message.classList.remove("msg-show");
+    }, 3500);
+  }
+  else {
+    pExpence += parseInt(inputAmount.value);
+    title_expence.textContent = pExpence;
+    title_balance.textContent = (
+      parseInt(title_balance.textContent) - parseInt(inputAmount.value)
+    ).toString();
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${inputText.value}</td>
+    <td>${inputAmount.value}</td>
+    <td>
+        <button value="1" class="btn-style edit-color"><i class="fas fa-edit"></i></button>
+        <button value="1" class="btn-style delete-color"><i class="fas fa-trash"></i></button>
+    </td>`
+    tbody.appendChild(tr);
+    inputText.value = " ";
+    inputAmount.value = "";
+  }
+
 }
 
-function deleteRow(e) {
-  let price = e.target.parentElement.previousSibling.textContent;
-  title_expence.textContent = `${(pExpence -= price)}`;
-  title_balance.textContent = (
-    parseInt(title_balance.textContent) + parseInt(price)
-  ).toString();
-  e.target.parentElement.parentElement.remove();
-}
 
-function editRow(e) {
-  inputText.value =
-    e.target.parentElement.parentElement.children[0].textContent;
-  inputAmount.value = parseInt(
-    e.target.parentElement.parentElement.children[1].textContent
-  );
-  let price = e.target.parentElement.previousSibling.textContent;
-  title_expence.textContent = `${(pExpence -= price)}`;
-  title_balance.textContent = (
-    parseInt(title_balance.textContent) + parseInt(price)
-  ).toString();
-  e.target.parentElement.parentElement.remove();
-}
+
+tbody.addEventListener('click', (e) => {
+  if (e.target.classList[1] == "delete-color") {
+    console.log(e.target.parentElement);
+    let price = e.target.parentElement.previousSibling.textContent;
+    title_expence.textContent = `${(pExpence -= price)}`;
+    title_balance.textContent = (
+      parseInt(title_balance.textContent) + parseInt(price)
+    ).toString();
+    e.target.parentElement.parentElement.remove();
+  }
+
+  else if (e.target.classList[1] == "edit-color") {
+    console.log(e.target.parentElement);
+    inputText.value =
+      e.target.parentElement.parentElement.children[0].textContent;
+    inputAmount.value = parseInt(
+      e.target.parentElement.parentElement.children[1].textContent
+    );
+    let price = e.target.parentElement.previousSibling.textContent;
+    title_expence.textContent = `${(pExpence -= price)}`;
+    title_balance.textContent = (
+      parseInt(title_balance.textContent) + parseInt(price)
+    ).toString();
+    e.target.parentElement.parentElement.remove();
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
