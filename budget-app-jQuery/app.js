b63492f35065f21d9d608fdda1681c433a68abab
sdt-soufian <sdt.soufian@gmail.com>
2021-08-10 00:21:00 +0100

    form validation with jQuery using jbox pluging

--- budget-app-jQuery/app.js
+++ budget-app-jQuery/app.js
@@ -1,5 +1,33 @@
-$(window).on('load', function(){
-    $('#btn-budget').on('click', function(){
-        console.log('bonsoir jQuery');
+$(window).on('load', function () {
+    var msg = new jBox('Modal', {
+        content: 'Error budget positive and great than 0',
+        //attach: '#btn-budget',
+        title: 'Error message',
+        draggable: 'title',
+        overlay: false,
+        repositionOnOpen: false,
+        repositionOnContent: false,
+        position: {
+            x: 25,
+            y: 10
+          }
+    });
+    $('#btn-budget').on('click', function (event) {
+        event.preventDefault();
+        if ($('#budget').val() == '' || $('#budget').val() < 0) {
+            console.log($(this));
+            msg.toggle();
+        }
+
     })
+
+    $('#btn-expense').on('click', function (event) {
+        event.preventDefault();
+        if ($('#expense-text').val() == '' || $('#Amount').val() < 0 || $('#Amount').val() == '') {
+            console.log($(this));
+            msg.toggle();
+        }
+
+    })
+
 })
